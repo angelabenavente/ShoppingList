b5c480ce16876944215a6ccadb0fd50e7fe665eb
angelabenavente <angela.b.almazan@gmail.com>
2020-01-22 13:27:26 +0100

    Add new items functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function() { 
 
+  $('form').submit(function(event){
+    event.preventDefault();
+  });
+
+  $('#addItemContainer').on('click', 'button', function() {
+    var value = $('#addItemContainer input').val();
+    var newItem = '<li class="item"> \
+      <div class="remove">X</div>' + value + '</li>'
+
+    $('#itemsContainer').append(newItem);
+  })
 
 })
