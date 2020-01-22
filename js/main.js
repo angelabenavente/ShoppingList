67be42fe715be57dec857beb7bcc7b0e291ad7c1
angelabenavente <angela.b.almazan@gmail.com>
2020-01-22 23:09:07 +0100

    finish basic version

--- js/main.js
+++ js/main.js
@@ -4,9 +4,19 @@ $(document).ready(function() {
     event.preventDefault();
   });
 
+
+  $('#addListContainer').on('change', '.addListButton', function() {
+    var val = $('#addListContainer input').val();
+    var newList = '<div class="singleListWrapper"><div class="addItemWrapper" id="addItemContainer"><form class="addItemForm"><input class="addItemInput" type="text" placeholder="Ej: Milk" /><button class="addItemButton">Add</button></form></div><ul class="itemsList" id="itemsContainer"></div>';
+
+    // $('#itemsContainer').append(newItem);
+    // $('#itemsContainer').prepend(newItem); /* Add item in last place' */
+    $(newList).appendTo('main'); /* Other efficient method. */
+  })
+
   $('#addItemContainer').on('click', 'button', function() {
     var value = $('#addItemContainer input').val();
-    var newItem = '<li class="itemWrapper"><input type="checkbox" class="checkbox"></input><div class="item">' + value + '<div class="remove fas fa-times"></i></li>'
+    var newItem = '<li class="itemWrapper"><input type="checkbox" class="checkbox"></input><div class="item"><p class="newItem">'+value+'</p><div class="remove fas fa-times"></div></li>';
 
     // $('#itemsContainer').append(newItem);
     // $('#itemsContainer').prepend(newItem); /* Add item in last place' */
@@ -17,8 +27,8 @@ $(document).ready(function() {
     var parent = $(this).parent().parent().remove();
   })
 
-  $('.checkbox').on('click', function() {
-    $(this).parent().toggleClass('done');
-    console.log('hi');
-  })
+  // $('.checkbox').on('click', function() {
+  //     $(this).parent().children().last().children().first().toggleClass('done');
+  //   console.log('hi');
+  // })
 })
